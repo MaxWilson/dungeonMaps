07558e3cd806a86233cd47768fa8433bea07dbd9
Max Wilson <wilson.max@gmail.com>
2023-07-11 19:54:58 -0700

    Placeholder for proof of concept

---
+++ src/scratch/Script1.fsx
@@ -0,0 +1,4 @@
+(* First attempt at generating a dungeon, purely in F# form (not visual or even text)
+   The emphasis is on getting the domain model to have the right pieces for e.g. mana level,
+   different kinds of monsters, etc. It's just a throwaway proof of concept.
+*)
